d268d01a7f76e3661c67906e048e14adf217dd80
thatslifebro <sy980407@gmail.com>
2023-08-26 17:12:45 +0900

    memory barrier 개념

--- server/ServerCore/Program.cs
+++ server/ServerCore/Program.cs
@@ -2,38 +2,60 @@
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace ServerCore 
+
+namespace ServerCore
 {
     class Program
     {
+        //volatile 쓰면 밑에 메모리에 바로 작업하는 것 처럼 된다
+        static int x = 0;
+        static int y = 0;
+        static int r1 = 0;
+        static int r2 = 0;
+
+        static void Thread1()
+        {
+            y = 1;// Store y
+            Thread.MemoryBarrier(); // 이거하면 위에거를 실제 메모리에 먼저 작업 .
+            r1 = x;// load x
+
+        }
+
+        static void Thread2()
+        {
+            x = 1;// Store x
+            Thread.MemoryBarrier();
+            r2 = y; // load y
+        }
+
         static void Main(string[] args)
         {
-            int[,] arr = new int[10000, 10000];
+            int count = 0;
+            while (true)
             {
-                long now = DateTime.Now.Ticks;
-                for (int i=0; i<10000; i++)
-                {
-                    for(int j = 0; j < 10000; j++)
-                    {
-                        arr[i, j] = 1;
-                    }
-                }
-                long end = DateTime.Now.Ticks;
-                Console.WriteLine($"(x,y) 걸린시간 {end - now}");
-            }
+                count++;
+                x = y = r1 = r2 = 0;
 
-            {
-                long now = DateTime.Now.Ticks;
-                for (int i = 0; i < 10000; i++)
+                Task t1 = new Task(Thread1);
+                Task t2 = new Task(Thread2);
+                t1.Start();
+                t2.Start();
+
+                Task.WaitAll(t1, t2);
+
+                if(r1==0 && r2 == 0)
                 {
-                    for (int j = 0; j < 10000; j++)
-                    {
-                        arr[j, i] = 0;
-                    }
+                    break;
                 }
-                long end = DateTime.Now.Ticks;
-                Console.WriteLine($"(y,x) 걸린시간 {end - now}");
             }
+            Console.WriteLine($"{count}번만에 빠져나옴");
         }
     }
-} 
+    
+}
+
+// 쓰레드 내부에서 명령들의 순서가 달라질 수 있다. 하드웨어에 의해.
+// 순서대로 하기 위해 메모리베리어 .
+// Full memory barrier : asm에선 MFENCE. C#에선 MemoryBerrier() => Store, load 둘다 막음
+// Store memory barrier : asm - SFENCE.
+// Load memory barrier  : asm - LFEENCE.
