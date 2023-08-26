77b6ac7bea906c071c71eff0259bc5e537e5e497
thatslifebro <sy980407@gmail.com>
2023-08-26 17:54:11 +0900

    경합조건(race condition) 개념, interlocked로 atomic하게 만들

--- server/ServerCore/Program.cs
+++ server/ServerCore/Program.cs
@@ -7,33 +7,50 @@ namespace ServerCore
 {
     class Program
     {
-        int _answer;
-        bool _complete;
+        static volatile int number = 0;
 
-        void A()
+        static void Thread_1()
         {
-            _answer = 123;
-            Thread.MemoryBarrier(); //1 store 
-            _complete = true;
-            Thread.MemoryBarrier(); //2 store
+            for(int i=0; i < 1000000; i++)
+            {
+                Interlocked.Increment(ref number);
+                //number++;// int temp = number; temp+=1; number=temp;
+            }
         }
 
-        void B()
+        static void Thread_2()
         {
-            Thread.MemoryBarrier(); //3 load 전 
-            if (_complete)
+            for(int i = 0; i < 1000000; i++)
             {
-                Thread.MemoryBarrier();//4 load 전
-                Console.WriteLine(_answer);
+                Interlocked.Decrement(ref number);
+                //number--;// int temp = number; temp+=1; number=temp;
             }
         }
 
         static void Main(string[] args)
         {
             
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine(number);
         }
     }
     
 }
-// barrier 가 4개 모두 있어야 정상작동.
-// store하고 난 후 , load 전.
+
+// 결과가 0이 안나옴 . 경합조건(race condition) 쓰레드들이 동시에 같은 변수에 접근한것 ?
+
+// 일단 ++ 할때 number 값을 temp에 저장 ++하고 다시 number에 저장한다
+
+// 한번에 저거 3개를 해야 정상작동인데. 그러지 못해서 문제.
+
+// 원자성 - atomic 하지 않아서 문제.
+
+//interlocked 쓰면 원자성 만족 하지만 성능 떨어짐
+
+//ref 는 주소값. 
