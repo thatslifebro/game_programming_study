305582798365b43f6ff2fc70366b3e9ad9c0aed3
thatslifebro <sy980407@gmail.com>
2023-08-26 17:18:00 +0900

    memory barrier 개념2

--- server/ServerCore/Program.cs
+++ server/ServerCore/Program.cs
@@ -7,55 +7,33 @@ namespace ServerCore
 {
     class Program
     {
-        //volatile 쓰면 밑에 메모리에 바로 작업하는 것 처럼 된다
-        static int x = 0;
-        static int y = 0;
-        static int r1 = 0;
-        static int r2 = 0;
+        int _answer;
+        bool _complete;
 
-        static void Thread1()
+        void A()
         {
-            y = 1;// Store y
-            Thread.MemoryBarrier(); // 이거하면 위에거를 실제 메모리에 먼저 작업 .
-            r1 = x;// load x
-
+            _answer = 123;
+            Thread.MemoryBarrier(); //1 store 
+            _complete = true;
+            Thread.MemoryBarrier(); //2 store
         }
 
-        static void Thread2()
+        void B()
         {
-            x = 1;// Store x
-            Thread.MemoryBarrier();
-            r2 = y; // load y
+            Thread.MemoryBarrier(); //3 load 전 
+            if (_complete)
+            {
+                Thread.MemoryBarrier();//4 load 전
+                Console.WriteLine(_answer);
+            }
         }
 
         static void Main(string[] args)
         {
-            int count = 0;
-            while (true)
-            {
-                count++;
-                x = y = r1 = r2 = 0;
-
-                Task t1 = new Task(Thread1);
-                Task t2 = new Task(Thread2);
-                t1.Start();
-                t2.Start();
-
-                Task.WaitAll(t1, t2);
-
-                if(r1==0 && r2 == 0)
-                {
-                    break;
-                }
-            }
-            Console.WriteLine($"{count}번만에 빠져나옴");
+            
         }
     }
     
 }
-
-// 쓰레드 내부에서 명령들의 순서가 달라질 수 있다. 하드웨어에 의해.
-// 순서대로 하기 위해 메모리베리어 .
-// Full memory barrier : asm에선 MFENCE. C#에선 MemoryBerrier() => Store, load 둘다 막음
-// Store memory barrier : asm - SFENCE.
-// Load memory barrier  : asm - LFEENCE.
+// barrier 가 4개 모두 있어야 정상작동.
+// store하고 난 후 , load 전.
