de9f8bdc9e4225d50ff12d73f9730156830a79bf
thatslifebro <sy9804071@gmail.com>
2023-08-26 16:34:50 +0900

    game server study

---
+++ server/ServerCore/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[10000, 10000];
+            {
+                long now = DateTime.Now.Ticks;
+                for (int i=0; i<10000; i++)
+                {
+                    for(int j = 0; j < 10000; j++)
+                    {
+                        arr[i, j] = 1;
+                    }
+                }
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(x,y) 걸린시간 {end - now}");
+            }
+
+            {
+                long now = DateTime.Now.Ticks;
+                for (int i = 0; i < 10000; i++)
+                {
+                    for (int j = 0; j < 10000; j++)
+                    {
+                        arr[j, i] = 0;
+                    }
+                }
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(y,x) 걸린시간 {end - now}");
+            }
+        }
+    }
+} 
