6565044734725896f74a319ed3d7b9f96b3ffdb1
thatslifebro <sy980407@gmail.com>
2023-08-30 17:05:52 +0900

    mutex

--- server/ServerCore/Program.cs
+++ server/ServerCore/Program.cs
@@ -5,48 +5,29 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    class Lock
-    {
-        // kernel 단에서 관리하는 bool 값.
-        //AutoResetEvent _available = new AutoResetEvent(true);//initial state true or false.
-        ManualResetEvent _available = new ManualResetEvent(true);
-        public void Acquire()
-        {
-
-            _available.WaitOne();// 입장 시도. auto reset event는 문닫기 자동.
-            _available.Reset(); //이라는게 원래있음 문닫기
-            // 이렇게 따로 하면 원자성이 만족안되서 return 값이 0이 안됨.
-            // lock 구현 시나리오와는 맞지 않음 (manual reset event)
-            
-        }
-        public void Release()
-        {
-            _available.Set(); // 문열기 .
-        }
-    }
 
     class Program
     {
         static int _num = 0;
-        static Lock _lock = new Lock();
+        static Mutex _lock = new Mutex();
 
         static void Thread_1()
         {
-            for(int i = 0; i < 100000; i++)
+            for(int i = 0; i < 1000000; i++)
             {
-                _lock.Acquire();
+                _lock.WaitOne();
                 _num++;
-                _lock.Release();
+                _lock.ReleaseMutex();
             }
         }
 
         static void Thread_2()
         {
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 1000000; i++)
             {
-                _lock.Acquire();
+                _lock.WaitOne();
                 _num--;
-                _lock.Release();
+                _lock.ReleaseMutex();
             }
         }
 
@@ -65,7 +46,6 @@ namespace ServerCore
     }
     
 }
-
-//제 3자가 lock이풀렷는지 확인해주고 알려주는 방법 : 제 3자가 kernel 쪽 사람이라 느림.(context switching이 필요하니)
-//auto reseet event, manual reeset event
-//다른 것들의 비해 느림.
+// mutex : 이것도 kernel단 까지 가는 거임. reset event 랑 비슷함
+//reset event는 boolean하나 저장하지만 mutex는 lock 개수를 셀수도 잇고, thread id도 있어서
+//누가 lock 걸었는지 이런 추가 정보들을 저장하고 있다.
