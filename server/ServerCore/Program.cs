6b7cf38ccf68d4314a9e051e271d1877a8ac036a
thatslifebro <sy980407@gmail.com>
2023-09-03 14:30:18 +0900

    lock 종류

--- server/ServerCore/Program.cs
+++ server/ServerCore/Program.cs
@@ -8,44 +8,58 @@ namespace ServerCore
 
     class Program
     {
-        static int _num = 0;
-        static Mutex _lock = new Mutex();
+        // 1. 근성  spinlock
+        // 2. 양보  sleep, yield
+        // 3. 갑질  auto reset event, mutex
+        //     monitor class
+        static object _lock = new object();
+        static SpinLock _lock2 = new SpinLock();
+        static Mutex _lock3 = new Mutex();
+        //or 직접 만들기
 
-        static void Thread_1()
+
+        class Reward
         {
-            for(int i = 0; i < 1000000; i++)
-            {
-                _lock.WaitOne();
-                _num++;
-                _lock.ReleaseMutex();
-            }
+
+        }
+        // 보상 목록 : [][][]  [][] 이렇게 있는데 여기 접근할 때 lock을 쓰면 동시다발적으로
+        //받기가 어려움. 보상을 추가하거나 변경할 때, 즉 write 할 때만 lock을 걸면 좋음.
+        // 개별로 거는 락, RWlock, reader writer lock
+        static ReaderWriterLockSlim _lock_slim = new ReaderWriterLockSlim();
+        static Reward GetRewardById(int id)
+        {
+            _lock_slim.EnterReadLock();
+            _lock_slim.ExitReadLock();
+            //writelock이 없으면 락이없는 것 처럼 동시다발적으로 읽기 가능.
+            return null;
         }
 
-        static void Thread_2()
+        void AddReward(Reward reward)
         {
-            for (int i = 0; i < 1000000; i++)
-            {
-                _lock.WaitOne();
-                _num--;
-                _lock.ReleaseMutex();
-            }
+            _lock_slim.EnterWriteLock();
+            _lock_slim.ExitWriteLock();
         }
+
 
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
-            t1.Start();
-            t2.Start();
+            lock (_lock)
+            {
 
-            Task.WaitAll(t1, t2);
+            }
 
-            Console.WriteLine(_num);
-            
+            // spinlock 이지만 계속 해도 안되면 잠깐 쉬다옴 
+            bool lockTaken = false;
+            try
+            {
+                _lock2.Enter(ref lockTaken);
+            }
+            finally
+            {
+                if(lockTaken)
+                    _lock2.Exit();
+            }
         }
     }
-    
+
 }
-// mutex : 이것도 kernel단 까지 가는 거임. reset event 랑 비슷함
-//reset event는 boolean하나 저장하지만 mutex는 lock 개수를 셀수도 잇고, thread id도 있어서
-//누가 lock 걸었는지 이런 추가 정보들을 저장하고 있다.
