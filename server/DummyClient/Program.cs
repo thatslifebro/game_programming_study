749486fdddd84d64b537d2fd1d13f51d52e03380
thatslifebro <sy980407@gmail.com>
2023-09-05 16:28:14 +0900

    socket programming

--- server/DummyClient/Program.cs
+++ server/DummyClient/Program.cs
@@ -1,3 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
 
+namespace DummyClient
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //DNS
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                Thread.Sleep(100);
+                socket.Connect(endPoint);
+                Console.WriteLine($"connect to {socket.RemoteEndPoint.ToString()}");
+
+                //send
+                byte[] sendBuff = Encoding.UTF8.GetBytes("hello this is client");
+                int sendBytes = socket.Send(sendBuff);
+
+                //recv
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = socket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"from server : {recvData.ToString()}");
+
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            
+
+        }
+
+    }
+}
+//socket programming ex
--- server/ServerCore/Program.cs
+++ server/ServerCore/Program.cs
@@ -1,43 +1,60 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
-
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
 
 namespace ServerCore
 {
 
     class Program
     {
-        //static string ThreadName; // 모든 쓰레드가 공유
-        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(() => { return $"{Thread.CurrentThread.ManagedThreadId}"; });//ThreadName.Value가 null일 때 이부분이 실행되는 것
-
-        static void WhoAmI()
-        {
-            //ThreadName.Value = $"My name is {Thread.CurrentThread.ManagedThreadId}";
-            bool repeat = ThreadName.IsValueCreated;
-            if (repeat)
-            {
-                Console.WriteLine(ThreadName.Value+"repeat");
-            } else
-            {
-                Console.WriteLine(ThreadName.Value);
-            }
-            
-
-            
-        }
-
+        
         static void Main(string[] args)
         {
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(3, 3);
-            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
+            //DNS
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
+            Socket listenSocket = new Socket(endPoint.AddressFamily,SocketType.Stream,ProtocolType.Tcp);
 
-            ThreadName.Dispose();
+            try
+            {
+                listenSocket.Bind(endPoint);
+
+                listenSocket.Listen(10); //동시 접속
+
+                while (true)
+                {
+                    Console.WriteLine("Listenning...");
+
+                    Socket clientSocket = listenSocket.Accept(); //blocking 넘어가지 못하면 대기 게임에선 blocking으로 처리되면 안됌 
+
+                    //recv
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = clientSocket.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"from client : {recvData}");
+
+                    //send
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("welcome to server!");
+                    clientSocket.Send(sendBuff);
+
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 
 }
 
-//tls
+//socket programming ex
